e6090e1ad3b6c79ea8dffbc22162254af9093153
Dorival <dorival.querino@gmail.com>
2020-09-25 13:38:34 -0300

    Health check url, new controller and port 5000

---
+++ src/main/java/br/com/jornada/cliente/controller/Health.java
@@ -0,0 +1,22 @@
+package br.com.jornada.cliente.controller;
+
+import br.com.jornada.cliente.domain.Cliente;
+import br.com.jornada.cliente.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class Health {
+
+    @GetMapping()
+    public String findAll(){
+        return "I am healthy";
+    }
+}
