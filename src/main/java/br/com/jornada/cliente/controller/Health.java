89355303b2f6fa77b23a09e855f02bd5cbdd0bf1
Dorival <dorival.querino@gmail.com>
2020-09-25 22:07:16 -0300

    Dependencias Restassured. Cliente-V6

--- src/main/java/br/com/jornada/cliente/controller/Health.java
+++ src/main/java/br/com/jornada/cliente/controller/Health.java
@@ -1,23 +1,14 @@
 package br.com.jornada.cliente.controller;
 
-import br.com.jornada.cliente.domain.Cliente;
-import br.com.jornada.cliente.service.ClienteService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.websocket.server.PathParam;
-import java.util.List;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Health {
 
     @GetMapping()
     public String findAll(){
-        return "<html><h2>Novo Pipeline - V5 </h2></html>";
+        return "Serviço de cliente-V6";
 		//return "forward: index.html";
     }
 }
--- src/test/java/br/com/jornada/cliente/ClienteApplicationTests.java
+++ src/test/java/br/com/jornada/cliente/ClienteApplicationTests.java
@@ -1,13 +1,37 @@
 package br.com.jornada.cliente;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+
+import org.apache.http.HttpStatus;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+import io.restassured.RestAssured;
+
+
+//@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT )
 class ClienteApplicationTests {
+	
+	@LocalServerPort
+	private int port;
+	
+	@Before
+	public void setUp() throws Exception {
+		RestAssured.baseURI = "http://localhost:" + port;
+	}
 
 	@Test
-	void contextLoads() {
+	public void confereVersaoTest(){
+		given()
+				.get("http://localhost:5000/")
+				.then()
+				.statusCode(HttpStatus.SC_OK)
+				.and()
+				.body(containsString("cliente-V6"));
 	}
 
 }
