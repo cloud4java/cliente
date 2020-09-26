8ba7d91e45cdb1824e412ccadad7a34ce9b46d29
Dorival <dorival.querino@gmail.com>
2020-09-26 16:09:22 -0300

    Versão 8

--- src/main/java/br/com/jornada/cliente/controller/Health.java
+++ src/main/java/br/com/jornada/cliente/controller/Health.java
@@ -8,6 +8,6 @@ public class Health {
 
     @GetMapping()
     public String findAll(){
-        return "Serviço de cliente-V7";
+        return "Serviço de cliente-V8";
     }
 }
--- src/test/java/br/com/jornada/cliente/ClienteApplicationTests.java
+++ src/test/java/br/com/jornada/cliente/ClienteApplicationTests.java
@@ -31,7 +31,7 @@ class ClienteApplicationTests {
 				.then()
 				.statusCode(HttpStatus.SC_OK)
 				.and()
-				.body(containsString("cliente-V7"));
+				.body(containsString("cliente-V8"));
 	}
 
 }
